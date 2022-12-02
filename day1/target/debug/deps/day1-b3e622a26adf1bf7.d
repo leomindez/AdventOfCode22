287af45bc27b6f9eff407a77e92249e0a3858dde
leo <leo@Mac-mini-de-leo.local>
2022-12-01 22:37:38 -0600

    Delete target folder

--- day1/target/debug/deps/day1-b3e622a26adf1bf7.d
+++
@@ -1,6 +0,0 @@
-/Users/leo/Documents/Projects/advent_code_2022/day1/target/debug/deps/day1-b3e622a26adf1bf7.rmeta: src/main.rs src/input
-
-/Users/leo/Documents/Projects/advent_code_2022/day1/target/debug/deps/day1-b3e622a26adf1bf7.d: src/main.rs src/input
-
-src/main.rs:
-src/input:
--- day1/target/debug/deps/day1-e84f0bb73c26a6ba.d
+++
@@ -1,6 +0,0 @@
-/Users/leo/Documents/Projects/advent_code_2022/day1/target/debug/deps/day1-e84f0bb73c26a6ba.rmeta: src/main.rs src/input
-
-/Users/leo/Documents/Projects/advent_code_2022/day1/target/debug/deps/day1-e84f0bb73c26a6ba.d: src/main.rs src/input
-
-src/main.rs:
-src/input:
